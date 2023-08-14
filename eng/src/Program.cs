0f705aa5de8ebdeaa5d672e6cfec7ac7d91cf61f
Antonín Procházka <antonin@postsharp.net>
2023-08-14 17:32:25 +0200

    Initial commit.

---
+++ eng/src/Program.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Solutions;
+using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
+using Spectre.Console.Cli;
+using Dependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.TemplateDependencies;
+
+var product = new Product( Dependencies.MetalamaConsolidated )
+{
+    Solutions = new Solution[]
+    {
+        new DotNetSolution( "src\\Metalama.Consolidated.sln" )
+    },
+    PublicArtifacts = Pattern.Create( "Metalama.Consolidated.$(PackageVersion).nupkg" ),
+    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering, MetalamaDependencies.V2023_3.Metalama }
+};
+
+var commandApp = new CommandApp();
+
+commandApp.AddProductCommands( product );
+
+return commandApp.Run( args );
