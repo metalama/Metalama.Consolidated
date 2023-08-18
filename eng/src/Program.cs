afbf6c73b5fb3a659c42ca89d4c7e7c7b44260a7
Antonín Procházka <antonin@postsharp.net>
2023-08-18 15:05:56 +0200

    #33615 Branch develop/2023.2 partially initialized.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -4,7 +4,7 @@ using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
 using Spectre.Console.Cli;
-using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2023_1;
+using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2023_2;
 
 var product = new Product( MetalamaDependencies.Consolidated )
 {
