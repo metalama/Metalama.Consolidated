8b33845116f6d3d929c94d850cd2afc3c7f43bec
Antonín Procházka <antonin@postsharp.net>
2024-03-28 20:13:03 +0100

    PostSharp.Engineering updated.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -427,7 +427,7 @@ object VersionBump : BuildType({
         schedule {
             schedulingPolicy = daily {
                 hour = 1
-                minute = 10
+                minute = 0
             }
             branchFilter = "+:<default>"
             triggerBuild = always()
