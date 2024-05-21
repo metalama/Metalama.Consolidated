93144145cd17369167d0c2debbca759cb7227c78
Antonín Procházka <antonin@postsharp.net>
2024-05-21 08:56:28 +0200

    #33656 Consolidated NuGet zip publishing.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,15 +1,41 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
+using BuildMetalamaConsolidated;
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Publishers;
+using PostSharp.Engineering.BuildTools.Build.Publishers.Downloads;
+using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
 using Spectre.Console.Cli;
 using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2024_0;
 
+var zipPackageName = "Metalama.$(PackageVersion).zip";
+var versionPackageName = "Metalama.Framework";
+var mainIndexName = "Index.xml";
+var packageIndexName = $"Index.{versionPackageName}.xml";
+
 var product = new Product( MetalamaDependencies.Consolidated )
 {
     IsBundle = true,
-    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering }
+    Solutions = [ new ConsolidatedBuildSolution( zipPackageName, versionPackageName ) ],
+    Dependencies = [ DevelopmentDependencies.PostSharpEngineering ],
+    Configurations = Product.DefaultConfigurations.WithValue(
+        BuildConfiguration.Public,
+        c => c with
+        {
+            PublicPublishers =
+            [
+                new DownloadsPublisher(
+                [
+                    S3Helper.CreateConfiguration( zipPackageName, MetalamaDependencies.Consolidated.ProductFamily ),
+                    S3Helper.CreateConfiguration( mainIndexName, MetalamaDependencies.Consolidated.ProductFamily ),
+                    S3Helper.CreateConfiguration( packageIndexName, MetalamaDependencies.Consolidated.ProductFamily )
+                ] ),
+                new MergePublisher()
+            ]
+        } )
 };
 
 var commandApp = new CommandApp();
