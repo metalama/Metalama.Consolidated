aa1799e6c97c4b13a5165b6a074ab9d00814febd
Gael Fraiteur <gael@postsharp.net>
2025-09-28 21:55:20 +0200

    Updated scripts.

--- scripts/UpdatePackageSuffix.ps1
+++ scripts/UpdatePackageSuffix.ps1
@@ -6,13 +6,17 @@ $suffixValue = if ($Suffix -eq "none") { "" } else { $Suffix }
 Get-ChildItem -Path $root -Recurse -File -Name "MainVersion.props" | ForEach-Object {
     $file = Join-Path $root $_
     $dir = Split-Path $file -Parent
+
+    Push-Location $dir
+
+    git pull
     
     Write-Host "Writing $_"
     [xml]$xml = Get-Content $file
     $xml.Project.PropertyGroup.PackageVersionSuffix = $suffixValue
     $xml.Save($file)
     
-    Push-Location $dir
+  
     git add MainVersion.props
     git commit -m "Set package version suffix to '$suffixValue'."
     git push
