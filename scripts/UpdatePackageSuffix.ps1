03b13ac37df6fb11ca091ab4cb45fe2dd030a4f2
Gael Fraiteur <gael@postsharp.net>
2025-09-28 21:59:07 +0200

    Updated scripts.

--- scripts/UpdatePackageSuffix.ps1
+++ scripts/UpdatePackageSuffix.ps1
@@ -3,13 +3,16 @@ param([Parameter(Mandatory=$true)][ValidateSet("none", "preview", "rc")][string]
 $root = Resolve-Path "$PSScriptRoot/../.."
 $suffixValue = if ($Suffix -eq "none") { "" } else { $Suffix }
 
-Get-ChildItem -Path $root -Recurse -File -Name "MainVersion.props" | ForEach-Object {
+Get-ChildItem -Path $root -Directory -Name "eng*" | ForEach-Object {
+    $engDir = Join-Path $root $_
+    Get-ChildItem -Path $engDir -File -Name "MainVersion.props"
+} | ForEach-Object {
     $file = Join-Path $root $_
     $dir = Split-Path $file -Parent
 
     Push-Location $dir
 
-    git pull
+    git pull --no-edit
     
     Write-Host "Writing $_"
     [xml]$xml = Get-Content $file
