9862644089cf9fdae37b81f9959fe40c973339a1
Gael Fraiteur <gael@postsharp.net>
2025-10-01 12:22:21 +0200

    Updated script version.

--- scripts/CheckoutDev.ps1
+++ scripts/CheckoutDev.ps1
@@ -16,9 +16,9 @@ foreach ($dir in $directories) {
 
     # Use develop/2025.1 for Metalama.Tests.NopCommerce, develop/2025.1 for others (can be customized as needed)
     if ($dir.Name -eq "Metalama.Tests.NopCommerce") {
-        git checkout dev/2025.2
+        git checkout dev/2026.0
     } else {
-        git checkout develop/2025.2
+        git checkout develop/2026.0
     }
     git pull --no-edit
 
