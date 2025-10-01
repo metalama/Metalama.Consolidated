0a3443c5aab3ee5fd07c4990a4c39773480ef6b9
Gael Fraiteur <gael@postsharp.net>
2025-10-01 12:15:51 +0200

    Added script.

---
+++ scripts/GenerateScripts.ps1
@@ -0,0 +1,22 @@
+# Get the absolute path of the root directory
+$rootPath = "$PSScriptRoot/../.."
+
+# Get all directories in the root path
+$directories = Get-ChildItem -Path $rootPath -Directory
+
+foreach ($dir in $directories) {
+
+    # Change to the directory
+    Set-Location $dir.FullName
+    
+    git pull --no-edit
+    & ./Build.ps1 generate-scripts
+    git commit --all -m "Update eng."
+    git push
+
+    Write-Host ""
+    Write-Host ""
+}
+
+# Return to the original directory
+Set-Location $rootPath
