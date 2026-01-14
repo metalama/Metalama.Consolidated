c4f68f5740144645ca8fab5b026658b5a8e90e1a
Gael Fraiteur <gael@postsharp.net>
2026-01-14 14:01:31 +0100

    Updating version number.

--- scripts/CheckoutDev.ps1
+++ scripts/CheckoutDev.ps1
@@ -16,9 +16,9 @@ foreach ($dir in $directories) {
 
     # Use develop/2025.1 for Metalama.Tests.NopCommerce, develop/2025.1 for others (can be customized as needed)
     if ($dir.Name -eq "Metalama.Tests.NopCommerce") {
-        git checkout dev/2026.0
+        git checkout dev/2026.1
     } else {
-        git checkout develop/2026.0
+        git checkout develop/2026.1
     }
     git pull --no-edit
 
