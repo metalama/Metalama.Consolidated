0897317696821aed52710698951888ba6be5b704
Gael Fraiteur <gael@postsharp.net>
2026-02-01 08:14:26 +0100

    Update eng.

--- scripts/UpdateEng.ps1
+++ scripts/UpdateEng.ps1
@@ -5,18 +5,23 @@ $rootPath = "$PSScriptRoot/../.."
 $directories = Get-ChildItem -Path $rootPath -Directory
 
 foreach ($dir in $directories) {
+    try {
+        # Change to the directory
+        Set-Location $dir.FullName
 
-    # Change to the directory
-    Set-Location $dir.FullName
-    
-    git pull --no-edit
-    & ./Build.ps1 dependencies update-eng
-    & ./Build.ps1 generate-scripts
-    git commit --all -m "Update eng."
-    git push
+        git pull --no-edit &&  `
+            & ./Build.ps1 dependencies update-eng  &&  `
+            & ./Build.ps1 dependencies reset PostSharp.Engineering &&  `
+            & ./Build.ps1 generate-scripts &&  `
+            git commit --all -m "Update eng."
 
-    Write-Host ""
-    Write-Host ""
+        Write-Host ""
+        Write-Host ""
+    }
+    catch {
+        Write-Host "Error processing $($dir.Name): $_" -ForegroundColor Red
+        Write-Host ""
+    }
 }
 
 # Return to the original directory
